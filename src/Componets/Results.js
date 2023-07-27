78115a98ca2134eea8624ca4fcd0035bba2d0426
Matthew Studzinski <88520969+MattStudzinski@users.noreply.github.com>
2023-07-26 22:15:07 -0700

    file works and is json

--- src/Componets/Results.js
+++ src/Componets/Results.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import XLSX from 'xlsx'
 
 const Results = () => {
+
+    const [fileName, setFileName] = useState(null)
+
+    const handleFile = async (e) => {
+        const file = e.target.files[0]
+        setFileName(file.name)
+        const data = await file.arrayBuffer();
+        const workbook = XLSX.read(data)
+        const worksheet = workbook.Sheets[workbook.SheetNames[0]]
+        const jsonData = XLSX.utils.sheet_to_json(worksheet)
+        console.log(jsonData)
+    }
+    
     return (
         <div>
-            results
+            {fileName && (
+                <p>File Name: <span>{fileName}</span></p>
+            )}
+            <input type="file" onChange= {(e) => handleFile(e)}/>
         </div>
     );
 };
