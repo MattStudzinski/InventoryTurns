b57f8d954ad34cc1c8417d3cfa20591730163b19
Matthew Studzinski <88520969+MattStudzinski@users.noreply.github.com>
2023-07-26 22:15:37 -0700

    pushed to correct arrays

--- src/Componets/Results.js
+++ src/Componets/Results.js
@@ -4,6 +4,7 @@ import XLSX from 'xlsx'
 const Results = () => {
 
     const [fileName, setFileName] = useState(null)
+    const [data, setData] =useState([])
 
     const handleFile = async (e) => {
         const file = e.target.files[0]
@@ -12,16 +13,35 @@ const Results = () => {
         const workbook = XLSX.read(data)
         const worksheet = workbook.Sheets[workbook.SheetNames[0]]
         const jsonData = XLSX.utils.sheet_to_json(worksheet)
-        console.log(jsonData)
+        setData(jsonData)
     }
+    console.log(data)
+
+    const greenArray = []
+    const orangeArray = []
+    const redArray = []
     
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].PUSAGE === 0 || data[i].PMVMNT <= .19) {
+            redArray.push(data[i])
+        } else if (data[i].PMVMNT <= .3){
+            orangeArray.push(data[i])
+        } else{ greenArray.push(data[i])}
+    }
+    console.log(greenArray)
+    console.log(orangeArray)
+    console.log(redArray)
     return (
+        <>
         <div>
             {fileName && (
                 <p>File Name: <span>{fileName}</span></p>
             )}
             <input type="file" onChange= {(e) => handleFile(e)}/>
+            
         </div>
+        
+        </>
     );
 };
 
